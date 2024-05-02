6a304c62b380061fe0741e4b1b86c24df0b08fd0
Antonio Brasilio <antonioalves05555@gmail.com>
2024-05-02 02:45:35 -0300

    feat: Filter complete

--- src/app/services/http-requests.service.ts
+++ src/app/services/http-requests.service.ts
@@ -13,7 +13,7 @@ export class HttpRequestsService {
             'https://ott-details.p.rapidapi.com/advancedsearch?start_year=1970&end_year=2020&min_imdb=6&max_imdb=7.8&genre=action&language=english&type=movie&sort=latest&page=1';
         const headers = new HttpHeaders({
             'X-RapidAPI-Key':
-                '2306b93b9cmsh16f4846061ad023p1d778cjsn2037ff385542',
+                'd2b3b5f552mshd1103942dcd34c3p122521jsnddfd2821eaa1',
             'X-RapidAPI-Host': 'ott-details.p.rapidapi.com',
         });
 
--- src/app/templates/home/home.component.ts
+++ src/app/templates/home/home.component.ts
@@ -14,11 +14,23 @@ export class HomeComponent implements OnInit {
     constructor(private httpRequestsService: HttpRequestsService) {}
 
     moviesList: any = [];
-    searchFilter: string = 'dasdsadsa';
+    searchFilter: string = '';
+    filteredMoviesList: any = [];
 
     ngOnInit() {
         this.httpRequestsService.getData().subscribe((data) => {
             this.moviesList = data.results;
+            this.filteredMoviesList = data.results;
+        });
+    }
+
+    filterMovies(event: any) {
+        this.filteredMoviesList = this.moviesList.filter((movie: any) => {
+            if (event !== '') {
+                return movie.title.toLowerCase().includes(event.toLowerCase());
+            }
+
+            return true;
         });
     }
 }
